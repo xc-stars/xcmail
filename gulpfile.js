6babc02769d76e1e021055614bd7e4baf4fa0bbd
qq245521957 <245521957@qq.com>
2016-10-26 21:37:41 +0800

    添加gulp-less，自动监听less

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp=require('gulp');
+var less=require('gulp-less');
+var lessSrc='static/less/**/*.less';
+gulp.watch(lessSrc,['compile-less'])	
+gulp.task('default',['compile-less'])
+gulp.task('compile-less',function(){
+  console.log('正在重新编译less')
+  gulp.src(lessSrc)
+      .pipe(less())
+      .pipe(gulp.dest('static/css'))
+})
